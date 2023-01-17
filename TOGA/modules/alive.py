2c5719c0462add2f966f6a57deb64fda4685d990
S L A D E <sladexsarcasm@gmail.com>
2023-01-18 01:46:40 +0530

    K

--- TOGA/modules/alive.py
+++ TOGA/modules/alive.py
@@ -9,15 +9,15 @@ from TOGA.events import register
 from TOGA import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/8f2641bc79d9b28a275e2.jpg"
+PHOTO = "https://te.legra.ph/file/82265204b556d514a3a1b.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  text2 = f"**Heyaa.. Weeb! [{event.sender.first_name}](tg://user?id={event.sender.id}), I'M Shanks.** \n\n"
+  text2 = f"**Heyaa.. Weeb! [{event.sender.first_name}](tg://user?id={event.sender.id}), I'M Akira.** \n\n"
   text2 += "⁜ **I'll Be Giving My Best For Your Work !!** \n\n"
   text2 += f"⁜ **Library Version :** `{telever}` \n\n"
   text2 += f"⁜ **Telethon Version :** `{tlhver}` \n\n"
   text2 += f"⁜ **Pyrogram Version :** `{pyrover}` \n\n"
   text2 += "**⁜Thanks For Adding Me Here ⁜**"
-  BUTTON = [[Button.url("ᴜᴘᴅᴀᴛᴇs", "https://t.me/shanks_UPDATES"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/AstorSupport")]]
+  BUTTON = [[Button.url("ᴜᴘᴅᴀᴛᴇs", "https://t.me/AstorPro"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/AstorSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=text2,  buttons=BUTTON)
